f1425bd5b637497bb0d2f81e434c8c46f8ad010f
Ernesto Canquiz <fdsoil123@gmail.com>
2024-04-19 19:46:59 -0400

    feat: Change design pattern

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,53 @@
-import './style.css'
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("button clicked");
+  formValidation();
+});
+
+const formValidation = () => {
+  if (input.value === "") {
+    msg.innerHTML = "Task cannot be blank";
+    console.log("failure");
+  } else {
+    console.log("successs");
+    msg.innerHTML = "";
+    acceptData();
+  }
+};
+
+let data = {};
+
+const acceptData = () => {
+  data["text"] = input.value;
+  console.log(data);
+  createTask();
+};
+
+const createTask = () => {
+  tasks.innerHTML += `
+  <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onClick="editTask(this)" class="fas fa-edit"></i>
+      <i onClick="deleteTask(this)" class="fas fa-trash-alt"></i>
+    </span>
+  </div>
+  `;
+  input.value = "";
+};
+
+const deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
+};
+
+const editTask = (e) => {
+  input.value = e.parentElement.previousElementSibling.innerHTML;
+  e.parentElement.parentElement.remove();
+};
+
+
